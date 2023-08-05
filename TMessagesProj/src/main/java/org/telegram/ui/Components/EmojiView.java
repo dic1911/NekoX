961963aa374df0d772edfab659b2b5c4f6bc1c10
xaxtix <xardas3200@gmail.com>
2023-08-05 11:32:46 +0400

    update to 9.7.6 (3721)

--- TMessagesProj/src/main/java/org/telegram/ui/Components/EmojiView.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/EmojiView.java
@@ -5744,6 +5744,7 @@ public class EmojiView extends FrameLayout implements NotificationCenter.Notific
         if (colorPickerView != null && colorPickerView.isShowing()) {
             colorPickerView.dismiss();
         }
+        ContentPreviewViewer.getInstance().clearDelegate(contentPreviewViewerDelegate);
     }
 
     private void checkDocuments(boolean isGif) {
--- TMessagesProj/src/main/java/org/telegram/ui/Components/spoilers/SpoilerEffect.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/spoilers/SpoilerEffect.java
@@ -300,7 +300,6 @@ public class SpoilerEffect extends Drawable {
             int dt = (int) Math.min(curTime - lastDrawTime, renderDelayMs);
             boolean hasAnimator = false;
 
-
             lastDrawTime = curTime;
 
             int left = getBounds().left, top = getBounds().top, right = getBounds().right, bottom = getBounds().bottom;
