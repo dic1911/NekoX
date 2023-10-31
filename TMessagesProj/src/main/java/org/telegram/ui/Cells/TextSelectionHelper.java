2124c257b7a1af9649ac340ba55194708b845b3e
dkaraush <dkaraush@gmail.com>
2023-10-31 21:13:31 +0400

    update to 10.2.2 (4071)

--- TMessagesProj/src/main/java/org/telegram/ui/Cells/TextSelectionHelper.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Cells/TextSelectionHelper.java
@@ -1931,7 +1931,6 @@ public abstract class TextSelectionHelper<Cell extends TextSelectionHelper.Selec
             if (super.callback != null) {
                 super.callback.onStateChanged(true);
             }
-            movingDirectionSettling = true;
             movingOffsetY = 0;
             movingOffsetX = 0;
             onOffsetChanged();
@@ -2154,8 +2153,7 @@ public abstract class TextSelectionHelper<Cell extends TextSelectionHelper.Selec
                 }
             }
             if (line >= 0) {
-                int k = layoutBlock.charOffset + layout.getOffsetForHorizontal(line, x);;
-                return k;
+                return layoutBlock.charOffset + layout.getOffsetForHorizontal(line, x);
             }
 
             return -1;
