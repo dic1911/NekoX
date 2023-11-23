0ec1415552d6734e9a75e682355a0925da364936
dic1911 <d69873@gmail.com>
2023-11-24 01:49:33 +0800

    use official icon set

--- TMessagesProj/src/main/java/org/telegram/ui/Adapters/DrawerLayoutAdapter.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Adapters/DrawerLayoutAdapter.java
@@ -301,23 +301,46 @@ public class DrawerLayoutAdapter extends RecyclerListView.SelectionAdapter imple
         int helpIcon;
         int peopleNearbyIcon;
         if (eventType == 0) {
+            newGroupIcon = R.drawable.msg_groups_ny;
             //newSecretIcon = R.drawable.msg_secret_ny;
             //newChannelIcon = R.drawable.msg_channel_ny;
+            contactsIcon = R.drawable.msg_contacts_ny;
+            callsIcon = R.drawable.msg_calls_ny;
+            savedIcon = R.drawable.msg_saved_ny;
+            settingsIcon = R.drawable.msg_settings_ny;
+            inviteIcon = R.drawable.msg_invite_ny;
+            helpIcon = R.drawable.msg_help_ny;
+            peopleNearbyIcon = R.drawable.msg_nearby_ny;
         } else if (eventType == 1) {
+            newGroupIcon = R.drawable.msg_groups_14;
             //newSecretIcon = R.drawable.msg_secret_14;
             //newChannelIcon = R.drawable.msg_channel_14;
+            contactsIcon = R.drawable.msg_contacts_14;
+            callsIcon = R.drawable.msg_calls_14;
+            savedIcon = R.drawable.msg_saved_14;
+            settingsIcon = R.drawable.msg_settings_14;
             inviteIcon = R.drawable.msg_secret_ny;
             helpIcon = R.drawable.msg_help;
             peopleNearbyIcon = R.drawable.msg_secret_14;
         } else if (eventType == 2) {
+            newGroupIcon = R.drawable.msg_groups_hw;
             //newSecretIcon = R.drawable.msg_secret_hw;
             //newChannelIcon = R.drawable.msg_channel_hw;
+            contactsIcon = R.drawable.msg_contacts_hw;
+            callsIcon = R.drawable.msg_calls_hw;
+            savedIcon = R.drawable.msg_saved_hw;
+            settingsIcon = R.drawable.msg_settings_hw;
             inviteIcon = R.drawable.msg_invite_hw;
             helpIcon = R.drawable.msg_help_hw;
             peopleNearbyIcon = R.drawable.msg_secret_hw;
         } else {
+            newGroupIcon = R.drawable.msg_groups;
             //newSecretIcon = R.drawable.msg_secret;
             //newChannelIcon = R.drawable.msg_channel;
+            contactsIcon = R.drawable.msg_contacts;
+            callsIcon = R.drawable.msg_calls;
+            savedIcon = R.drawable.msg_saved;
+            settingsIcon = R.drawable.msg_settings_old;
             inviteIcon = R.drawable.msg_invite;
             helpIcon = R.drawable.msg_help;
             peopleNearbyIcon = R.drawable.msg_nearby;
