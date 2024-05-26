3ba74d3107cc5c5accfe03a66e8ab485da871590
dic1911 <d69873@gmail.com>
2024-05-26 21:00:51 +0800

    feat: add new icon and preserve old one as an option
    icon credit: xaxions@Telegram & Nekogram

--- TMessagesProj/src/main/java/org/telegram/ui/LauncherIconController.java
+++ TMessagesProj/src/main/java/org/telegram/ui/LauncherIconController.java
@@ -34,8 +34,19 @@ public class LauncherIconController {
     }
 
     public enum LauncherIcon {
-        DEFAULT("DefaultIcon", R.drawable.ic_launcher_foreground, R.mipmap.ic_launcher, R.string.AppIconDefault),
-        OLD("OldIcon", R.drawable.ic_launcher_foreground, R.mipmap.ic_launcher_old, R.string.AppIconOld),
+        DEFAULT("DefaultIcon", R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.string.AppIconDefault),
+        COLORED("ColoredIcon", R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.string.AppIconColored),
+        OLD_CLASSIC("ClassicIcon", R.drawable.ic_launcher_classic_foreground, R.mipmap.ic_launcher_classic, R.string.AppIconColoredClassic),
+        THEMED_CLASSIC("ThemedClassicIcon", R.color.ic_launcher_background, R.drawable.icon_preview_neko_classic, R.string.AppIconThemedClassic),
+        THEMED_XEYE("ThemedXEyeIcon", R.color.ic_launcher_background, R.drawable.icon_preview_neko_xeye_foreground, R.string.AppIconThemedXEye),
+        NEKO_AQUA("NekoAquaIcon", R.drawable.icon_4_background_sa, R.drawable.icon_preview_neko_xeye_foreground, R.string.AppIconAqua),
+        NEKO_PREMIUM("NekoPremiumIcon", R.drawable.icon_3_background_sa, R.drawable.icon_preview_neko_wink_foreground, R.string.AppIconPremium),
+        NEKO_TURBO("NekoTurboIcon", R.drawable.icon_5_background_sa, R.drawable.icon_preview_neko_wink_foreground, R.string.AppIconTurbo),
+        NEKO_PUSHEEN("NekoPusheenIcon", R.mipmap.ic_launcher_pusheen_background, R.drawable.icon_neko_pusheen_foreground_round, R.string.AppIconPusheen),
+        NEKO_MUSHEEN("NekoMusheenIcon", R.mipmap.ic_launcher_musheen_background, R.drawable.icon_neko_musheen_foreground_round, R.string.AppIconMusheen),
+        NEKO_RAINBOW("NekoRainbowIcon", R.mipmap.ic_launcher_rainbow_background, R.drawable.icon_neko_rainbow_foreground_round, R.string.AppIconRainbow),
+        NEKO_SPACE("NekoSpaceIcon", R.mipmap.ic_launcher_space_background, R.drawable.icon_neko_space_foreground_round, R.string.AppIconSpace),
+        NEKO_NEON("NekoNeonIcon", R.mipmap.ic_launcher_neon_background, R.drawable.icon_neko_neon_foreground_round, R.string.AppIconNeon),
         VINTAGE("VintageIcon", R.drawable.icon_6_background_sa, R.mipmap.icon_6_foreground_sa, R.string.AppIconVintage),
         AQUA("AquaIcon", R.drawable.icon_4_background_sa, R.mipmap.icon_foreground_sa, R.string.AppIconAqua),
         PREMIUM("PremiumIcon", R.drawable.icon_3_background_sa, R.mipmap.icon_3_foreground_sa, R.string.AppIconPremium),
@@ -70,7 +81,7 @@ public class LauncherIconController {
         }
 
         public boolean isNekoX() {
-            return this == DEFAULT || this == OLD;
+            return this == DEFAULT || this == COLORED || this == OLD_CLASSIC;
         }
     }
 }
