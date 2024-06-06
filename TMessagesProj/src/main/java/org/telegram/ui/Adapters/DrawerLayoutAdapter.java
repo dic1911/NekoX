5a640c7fac94391bfdc6899e7f5a71719c02b314
dic1911 <d69873@gmail.com>
2024-06-06 09:40:54 +0800

    fix: default icon color under certain conditions

--- TMessagesProj/src/main/java/org/telegram/ui/Adapters/DrawerLayoutAdapter.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Adapters/DrawerLayoutAdapter.java
@@ -301,15 +301,15 @@ public class DrawerLayoutAdapter extends RecyclerListView.SelectionAdapter imple
         int helpIcon;
         int peopleNearbyIcon;
         if (eventType == 0) {
-            newGroupIcon = R.drawable.msg_groups_ny;
+//            newGroupIcon = R.drawable.msg_groups_ny;
             //newSecretIcon = R.drawable.msg_secret_ny;
             //newChannelIcon = R.drawable.msg_channel_ny;
-            contactsIcon = R.drawable.msg_contacts_ny;
-            callsIcon = R.drawable.msg_calls_ny;
-            savedIcon = R.drawable.msg_saved_ny;
-            settingsIcon = R.drawable.msg_settings_ny;
-            inviteIcon = R.drawable.msg_invite_ny;
-            helpIcon = R.drawable.msg_help_ny;
+//            contactsIcon = R.drawable.msg_contacts_ny;
+//            callsIcon = R.drawable.msg_calls_ny;
+//            savedIcon = R.drawable.msg_saved_ny;
+//            settingsIcon = R.drawable.msg_settings_ny;
+//            inviteIcon = R.drawable.msg_invite_ny;
+//            helpIcon = R.drawable.msg_help_ny;
             peopleNearbyIcon = R.drawable.msg_nearby_ny;
         } else if (eventType == 1) {
             newGroupIcon = R.drawable.msg_groups_14;
