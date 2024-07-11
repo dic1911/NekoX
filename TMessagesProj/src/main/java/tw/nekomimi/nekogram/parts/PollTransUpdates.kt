83a4262eb0719892d5f1cc54fe60ea8ae65e9398
dic1911 <d69873@gmail.com>
2024-07-11 09:24:33 +0800

    fix: translated poll caused NPE

--- TMessagesProj/src/main/java/tw/nekomimi/nekogram/parts/PollTransUpdates.kt
+++ TMessagesProj/src/main/java/tw/nekomimi/nekogram/parts/PollTransUpdates.kt
@@ -1,10 +1,20 @@
 package tw.nekomimi.nekogram.parts
 
 import org.telegram.tgnet.TLRPC
+import tw.nekomimi.nekogram.NekoConfig
+import tw.nekomimi.nekogram.transtale.TranslateDb
+import tw.nekomimi.nekogram.transtale.code2Locale
 
 fun postPollTrans(media: TLRPC.TL_messageMediaPoll, poll: TLRPC.TL_poll) {
     poll.translatedQuestion = media.poll.translatedQuestion
     poll.answers.forEach { answer ->
-        answer.translatedText = media.poll.answers.find { it.text == answer.text }!!.translatedText
+        val ans = media.poll.answers.find { it != null && it.text == answer.text }
+        if (ans != null && answer != null)
+            answer.translatedText = ans.translatedText
+        else if (answer != null) {
+            // workaround for null stuff
+            val db = TranslateDb.forLocale(NekoConfig.translateToLang.String().code2Locale)
+            answer.translatedText = db.query(answer.text.text) + " | " + answer.text.text
+        }
     }
 }
